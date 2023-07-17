c30056c9010068470d73d95e91d04494df37e789
Alexandra Ball <alex.ball4000@gmail.com>
2023-07-17 23:10:52 +0400

    making more commands

--- Functions.cpp
+++ Functions.cpp
@@ -10,12 +10,11 @@ Functions::~Functions( ) {
 
 void Functions::addNick( std::string nick ) {
 	try {
-		std::string err = ":Nickname " + nick + "! is already in use\n";
+		std::string err = "433 " + clients[fd].getNick() + " " + nick + " :Nickname is already in use\r\n";
 		nicks.at(nick);
 		send(fd, &err[0], err.length(), 0);
 	} catch ( std::exception &e ) {
-		// std::cout << AnytoStr(RPL_WELCOME) << std::endl;
-		std::string yes = "001 :Welcome You are now known as " + nick + "!user@host";
+		std::string yes = "001 :Welcome You are now known as " + nick + "!user@host\r\n";
 		nicks[nick] = clients[fd];
 		clients[fd].setNick(nick);
 		send(fd, &yes[0], yes.length(), 0);
@@ -31,6 +30,12 @@ void Functions::NICK( void ) {
 	this->findNick();
 }
 
+void Functions::CAP( void ) {
+
+}
+void Functions::JOIN( void ) {
+
+}
 
 // NICK aballer
 // USER user user host.docker.internal :Unknown
