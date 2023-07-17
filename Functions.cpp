5c20bf5e5502768976192db22e999d05f9ae516f
Alexandra Ball <alex.ball4000@gmail.com>
2023-07-17 23:34:15 +0400

    making more commands

--- Functions.cpp
+++ Functions.cpp
@@ -10,11 +10,11 @@ Functions::~Functions( ) {
 
 void Functions::addNick( std::string nick ) {
 	try {
-		std::string err = "433 " + clients[fd].getNick() + " " + nick + " :Nickname is already in use\r\n";
+		std::string err = ":localhost 433 " + clients[fd].getNick() + " " + nick + " :Nickname is already in use\r\n";
 		nicks.at(nick);
 		send(fd, &err[0], err.length(), 0);
 	} catch ( std::exception &e ) {
-		std::string yes = "001 :Welcome You are now known as " + nick + "!user@host\r\n";
+		std::string yes = ":localhost 001 :Welcome You are now known as " + nick + "!user@host\r\n";
 		nicks[nick] = clients[fd];
 		clients[fd].setNick(nick);
 		send(fd, &yes[0], yes.length(), 0);
