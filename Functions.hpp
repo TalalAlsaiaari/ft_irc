c7ba83daf16ff76311b9bd2a6e2952cad1aba599
Alexandra Ball <alex.ball4000@gmail.com>
2023-07-29 08:32:45 +0400

    now ignoring partial commands when cntrl D is pressed while typing

--- Functions.hpp
+++ Functions.hpp
@@ -1,13 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <string>
 #include <vector>
