cd53fbf8acd1eb0d839dbf489cb38eabf2c0b36b
Talal Alsaiaari <talsaiaa@student.42abudhabi.ae>
2023-08-13 19:06:52 +0400

    reordered commands to match modern.ircdocs reference

--- Commands.hpp
+++ Commands.hpp
@@ -4,21 +4,22 @@
 
 class Commands : public Functions {
     public:
-        void NICK( void );
 		void CAP( void );
-		void JOIN( void );
-		void USER( void );
-		void MODE( void );
-		void PING( void );
-		void PART( void );
-		void PRIVMSG( void );
-		void NOTICE( void );
 		void PASS( void );
-		void MOTD( void );
-		void QUIT( void );
-		void WHOIS( void );
+        void NICK( void );
+		void USER( void );
+		void PING( void );
 		void OPER(void);
-		void KILL (void);
+		void QUIT( void );
+		void JOIN( void );
+		void PART( void );
 		void TOPIC(void);
 		void INVITE(void);
+		void KICK(void);
+		void MOTD( void );
+		void MODE( void );
+		void PRIVMSG( void );
+		void NOTICE( void );
+		void WHOIS( void );
+		void KILL (void);
 };
