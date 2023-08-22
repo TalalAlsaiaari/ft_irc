bcfef49a1dbcf41132633b93805ce65fbab72eda
Talal Alsaiaari <talsaiaa@lab1r5s3.42abudhabi.ae>
2023-08-22 19:44:45 +0400

    organized functions.cpp/.hpp

--- Functions.hpp
+++ Functions.hpp
@@ -33,24 +33,33 @@ class Functions {
 		std::map<std::string, Client *> sent;
 		typedef std::map<std::string, Channel>::iterator chan_it;
 		typedef std::map<std::string, Client *>::iterator client_it;
+
 	public:
 		Functions( );
 		virtual ~Functions( );
-		virtual void takeInput( std::string input, int fd, Client &client ) = 0;
+
+		/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Setters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 		void setPass( std::string pass );
-		void updateChannel(Client &client, std::string old_nick, std::string new_nick);
+
+		/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Getters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 		std::string getPass( void ) const;
-		bool checkNick( std::string nick );
-		bool RegisterUser( void );
-		void quitMsg(Client source, std::string msg);
-		void killMsg(Client &source, Client &dest);
-		void errMsg(client_it dest, std::string msg);
-		std::vector<std::string> split(std::string str, std::string delim);
-		
+
+		/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Booleans ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 		bool isEnoughParams(unsigned int paramNum);
 		bool channelExist(std::string chanName, chan_it channel);
 		bool userInChan(std::string chanName, chan_it channel);
 		bool isChanName( std::string name );
+		bool checkNick( std::string nick );
+		bool RegisterUser( void );
+
+		/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 		void userMode(std::string modes, std::string name);
 		void channelMode(std::string modes, chan_it chan);
+		void updateChannel(Client &client, std::string old_nick, std::string new_nick);
+		std::vector<std::string> split(std::string str, std::string delim);
+		void quitMsg(Client source, std::string msg);
+		void killMsg(Client &source, Client &dest);
+		void errMsg(client_it dest, std::string msg);
+		
+		virtual void takeInput( std::string input, int fd, Client &client ) = 0;
 };
