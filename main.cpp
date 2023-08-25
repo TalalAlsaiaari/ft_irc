f27f358e3c99b0ab5240ec5e3231ff2c9ef86e78
Talal Alsaiaari <talsaiaa@lab1r5s3.42abudhabi.ae>
2023-08-25 18:51:24 +0400

    reserved ports are now handled

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ int	main(int ac, char **av)
 {
 	Server	server;
 
-    if (ac == 3) {
-        server.setPort(av[1]);
-        server.parser.setPass(av[2]);
-    } else {
-        std::cout << "expected params: <port> <password>\n";
-        return 1;
-    }
     try
 	{
-        server.ftIRC();
-    }
+        if (ac == 3) {
+            server.setPort(av[1]);
+            server.parser.setPass(av[2]);
+        } else {
+            std::cout << "expected params: <port> <password>\n";
+            return 1;
+        }
+            server.ftIRC();
+        }
     catch(std::exception &e)
     {
         std::cout << e.what() << std::endl;
